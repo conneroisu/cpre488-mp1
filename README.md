52ce12f7832919ba864fa63ad6f9cb3953013752
connerohnesorge <conneroisu@outlook.com>
2025-02-10 20:56:54 -0600

    batman

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # cpre488-mp1
+
 mp1
+
+## Developer Tooling
+
+[ccls](https://github.com/MaskRay/ccls)
+[nvc](https://github.com/nickg/nvc)
+[vhdl-ls](https://github.com/VHDL-LS/rust_hdl)
+[ghdl](https://github.com/ghdl/ghdl)
+
+### Configuration Files
+
+#### ccls
+
+At the root of the project exists a `.ccls` file. This file is used to configure ccls.
---
+++ flake.nix
@@ -0,0 +1,104 @@
+{
+  description = "MP1 CPRE488 Project";
+
+  inputs = {
+    devenv-root = {
+      url = "file+file:///dev/null";
+      flake = false;
+    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    devenv.url = "github:cachix/devenv";
+    nix2container.url = "github:nlewo/nix2container";
+    nix2container.inputs.nixpkgs.follows = "nixpkgs";
+    mk-shell-bin.url = "github:rrbutani/nix-mk-shell-bin";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = inputs @ {
+    self,
+    devenv-root,
+    nixpkgs,
+    ...
+  }:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        inputs.devenv.flakeModule
+      ];
+      systems = [
+        "x86_64-linux"
+        "i686-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      perSystem = {
+        config,
+        self',
+        inputs',
+        # pkgs,
+        system,
+        ...
+      }: {
+        devenv.shells.default = let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [];
+            config.allowUnfree = true;
+          };
+          # Rosetta is required to translate some packages macOS on Apple Silicon.
+          rosettaPkgs =
+            if isDarwin && isAarch64
+            then pkgs.pkgsx86_64Darwin
+            else pkgs;
+
+          inherit (pkgs) lib stdenv;
+          inherit (stdenv) isLinux isDarwin isAarch64;
+        in {
+          devenv.root = let
+            devenvRootFileContent = builtins.readFile devenv-root.outPath;
+          in
+            pkgs.lib.mkIf (devenvRootFileContent != "") devenvRootFileContent;
+
+          name = "mp1-devenv";
+          cachix.enable = true;
+
+          packages =
+            [
+              # Nix
+              pkgs.alejandra
+              pkgs.nixd
+              # C/C++
+              pkgs.ccls
+              pkgs.clang-tools
+              pkgs.zig
+              pkgs.gcc
+              pkgs.pkg-config
+              # Cross-Platform VHDL
+              pkgs.vhd2vl
+              pkgs.vhdl-ls
+            ]
+            ++ lib.optionals isLinux [
+              pkgs.ghdl
+              pkgs.nvc
+            ]
+            ++ lib.optionals isDarwin [
+              rosettaPkgs.ghdl
+              rosettaPkgs.nvc
+            ];
+
+          enterShell = ''export REPO_ROOT=$(git rev-parse --show-toplevel)'';
+
+          scripts = {
+            dx.exec = "$EDITOR $REPO_ROOT/flake.nix";
+          };
+        };
+      };
+      flake = {};
+    };
+}
