fa5174eb974d7e018805ff9e0e083e4dda7f89c7
connerohnesorge <conneroisu@outlook.com>
2025-02-17 14:32:53 -0600

    latest

--- src/testbench/generate_fsm/tb_generate_fsm.vhd
+++ src/testbench/generate_fsm/tb_generate_fsm.vhd
@@ -86,7 +86,7 @@ begin
          i_slv_reg23 <= std_logic_vector(to_unsigned(i, 32));
          i_slv_reg24 <= std_logic_vector(to_unsigned(i, 32));
          i_slv_reg25 <= std_logic_vector(to_unsigned(i, 32));
-         wait for 10 ns;
+         wait for 2000 ns;
       end loop;
 
       wait;
