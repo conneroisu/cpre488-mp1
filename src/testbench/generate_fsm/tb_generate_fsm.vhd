db048b69ab1422c265d28b51d9e9420742638e1c
connerohnesorge <conneroisu@outlook.com>
2025-02-18 07:47:56 -0600

    1 sec

--- src/testbench/generate_fsm/tb_generate_fsm.vhd
+++ src/testbench/generate_fsm/tb_generate_fsm.vhd
@@ -81,7 +81,7 @@ begin
         i_slv_reg24 <= std_logic_vector(to_unsigned(200000, 32));
         i_slv_reg25 <= std_logic_vector(to_unsigned(175000, 32));
 
-        wait for 5000000 ms;
+        wait for 1 sec;
 
         wait;
     end process;
