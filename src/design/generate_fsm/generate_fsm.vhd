01b3e4d149799ab165c80fecfe361eca9167fcb4
connerohnesorge <conneroisu@outlook.com>
2025-02-19 14:47:51 -0600

    added idle fram time generic

--- src/design/generate_fsm/generate_fsm.vhd
+++ src/design/generate_fsm/generate_fsm.vhd
@@ -3,10 +3,10 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
 entity generate_fsm is
-
     generic (
-        N : natural := 32
-        );
+        N               : natural := 32;
+        IDLE_FRAME_TIME : time    := 2 ms
+    );
     port (
         i_clk       : in  std_logic;
         i_rst       : in  std_logic;
@@ -18,8 +18,7 @@ entity generate_fsm is
         i_slv_reg24 : in  std_logic_vector(N-1 downto 0);
         i_slv_reg25 : in  std_logic_vector(N-1 downto 0);
         o_ppm       : out std_logic
-        );
-
+    );
 end generate_fsm;
 
 architecture arc of generate_fsm is
@@ -27,35 +26,44 @@ architecture arc of generate_fsm is
     type state_type is (IDLE, GAP, CHAN1, CHAN2, CHAN3, CHAN4, CHAN5, CHAN6);
     signal current_state : state_type := IDLE;
 
-    -- Counter signal used to generate delays.
+    -- Counter signals for delays and idle time
     signal delay_cntr : natural := 0;
     signal gap_cntr   : natural := 0;
+    signal idle_cntr  : natural := 0;
 
-    -- Clock period and gap delay parameters.
-    constant CLK_PERIOD   : time    := 10 ns;
-    -- 0.40 ms / 10 ns = 40,000 cycles
-    constant GAP_TIME_CNT : natural := integer(0.40 ms / CLK_PERIOD);
+    -- Clock period and delay parameters
+    constant CLK_PERIOD      : time    := 10 ns;
+    constant GAP_TIME_CNT    : natural := integer(0.40 ms / CLK_PERIOD);
+    constant IDLE_FRAME_CNT  : natural := integer(IDLE_FRAME_TIME / CLK_PERIOD);
 
 begin
 
-    process(i_clk, i_rst, o_ppm, delay_cntr, gap_cntr)
+    process(i_clk, i_rst, o_ppm, delay_cntr, gap_cntr, idle_cntr)
     begin
         if i_rst = '1' then
             current_state <= IDLE;
             delay_cntr    <= 0;
+            idle_cntr     <= 0;
             o_ppm         <= '0';
 
-        elsif (rising_edge(i_clk) and i_slv_reg0(0) = '1') then
+        elsif rising_edge(i_clk) then
             case current_state is
 
                 when IDLE =>
-                    o_ppm         <= '0';
-                    current_state <= CHAN1;
-                    delay_cntr    <= 0;
+                    o_ppm <= '0';
+                    
+                    -- Wait for idle frame period before allowing a new frame
+                    if idle_cntr < IDLE_FRAME_CNT then
+                        idle_cntr <= idle_cntr + 1;
+                        current_state <= IDLE;
+                    elsif i_slv_reg0(0) = '1' then
+                        idle_cntr <= 0;
+                        current_state <= CHAN1;
+                        delay_cntr <= 0;
+                    end if;
 
                 when CHAN1 =>
                     if delay_cntr + 1 < to_integer(unsigned(i_slv_reg20)) then
-                        current_state <= CHAN1;
                         delay_cntr    <= delay_cntr + 1;
                         o_ppm         <= '1';
                     else
@@ -66,8 +74,7 @@ begin
                     end if;
 
                 when CHAN2 =>
-                    if delay_cntr +1 < to_integer(unsigned(i_slv_reg21)) then
-                        current_state <= CHAN2;
+                    if delay_cntr + 1 < to_integer(unsigned(i_slv_reg21)) then
                         delay_cntr    <= delay_cntr + 1;
                         o_ppm         <= '1';
                     else
@@ -79,7 +86,6 @@ begin
 
                 when CHAN3 =>
                     if delay_cntr + 1 < to_integer(unsigned(i_slv_reg22)) then
-                        current_state <= CHAN3;
                         delay_cntr    <= delay_cntr + 1;
                         o_ppm         <= '1';
                     else
@@ -91,7 +97,6 @@ begin
 
                 when CHAN4 =>
                     if delay_cntr + 1 < to_integer(unsigned(i_slv_reg23)) then
-                        current_state <= CHAN4;
                         delay_cntr    <= delay_cntr + 1;
                         o_ppm         <= '1';
                     else
@@ -103,7 +108,6 @@ begin
 
                 when CHAN5 =>
                     if delay_cntr + 1 < to_integer(unsigned(i_slv_reg24)) then
-                        current_state <= CHAN5;
                         delay_cntr    <= delay_cntr + 1;
                         o_ppm         <= '1';
                     else
@@ -115,45 +119,36 @@ begin
 
                 when CHAN6 =>
                     if delay_cntr + 1 < to_integer(unsigned(i_slv_reg25)) then
-                        current_state <= CHAN6;
                         delay_cntr    <= delay_cntr + 1;
                         o_ppm         <= '1';
                     else
-                        current_state <= IDLE;
+                        current_state <= IDLE;  -- Transition directly to IDLE after last channel
                         delay_cntr    <= 0;
-                        gap_cntr      <= 1;
+                        idle_cntr     <= 0;  -- Reset idle counter to start counting the idle period
                         o_ppm         <= '0';
                     end if;
 
                 when GAP =>
                     if delay_cntr < GAP_TIME_CNT then
-                        current_state <= GAP;
                         delay_cntr    <= delay_cntr + 1;
                         o_ppm         <= '0';
                     else
                         delay_cntr <= 0;
                         o_ppm      <= '1';
                         case gap_cntr is
-                            when 0 =>
-                                current_state <= CHAN1;  -- TODO: sync gap?
-                            when 1 =>
-                                current_state <= CHAN2;
-                            when 2 =>
-                                current_state <= CHAN3;
-                            when 3 =>
-                                current_state <= CHAN4;
-                            when 4 =>
-                                current_state <= CHAN5;
-                            when 5 =>
-                                current_state <= CHAN6;
-                            when others =>
-                                current_state <= IDLE;
+                            when 1 => current_state <= CHAN2;
+                            when 2 => current_state <= CHAN3;
+                            when 3 => current_state <= CHAN4;
+                            when 4 => current_state <= CHAN5;
+                            when 5 => current_state <= CHAN6;
+                            when others => current_state <= IDLE;
                         end case;
                     end if;
 
                 when others =>
                     current_state <= IDLE;
                     delay_cntr    <= 0;
+                    idle_cntr     <= 0;
                     o_ppm         <= '0';
             end case;
         end if;
