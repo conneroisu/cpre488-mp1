417deacaa006c1bb57f84da8eafe93d2ca76ede3
conneroisu <conneroisu@outlook.com>
2025-02-24 10:01:06 -0600

    package ip 54

--- src/design/generate_fsm/generate_fsm.vhd
+++ src/design/generate_fsm/generate_fsm.vhd
@@ -3,11 +3,12 @@ USE IEEE.numeric_std.ALL;
 USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.STD_LOGIC_ARITH.ALL;
 USE IEEE.STD_LOGIC_UNSIGNED.ALL;
+USE work.user_defines.ALL;
 
 ENTITY generate_fsm IS
 
     GENERIC (
-        N : INTEGER := 32
+        N : INTEGER := REG_SIZE
     );
     PORT (
         i_clk : IN STD_LOGIC;
@@ -22,24 +23,11 @@ END generate_fsm;
 
 ARCHITECTURE Behavioral OF generate_fsm IS
 
-    TYPE pulse_width_array IS ARRAY(0 TO 5) OF STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
-    TYPE state_type IS (
-        IDLE, IDLE_LOW_PULSE,
-        CHANNEL,
-        GAP_LOW,
-        PULSE_HIGH,
-        NEXT_CHANNEL,
-        FRAME_COMPLETE
-    );
-
-    CONSTANT GAP_COUNT : STD_LOGIC_VECTOR(15 DOWNTO 0) := STD_LOGIC_VECTOR(to_unsigned(40000, 16));
-    CONSTANT FRAME_COUNT : STD_LOGIC_VECTOR(20 DOWNTO 0) := STD_LOGIC_VECTOR(to_unsigned(200000, 21));
-
-    SIGNAL PS, NS : state_type;
-    SIGNAL cycle_counter : STD_LOGIC_VECTOR(31 DOWNTO 0);
-    SIGNAL cycle_counter_total : STD_LOGIC_VECTOR(31 DOWNTO 0);
-    SIGNAL channel_index : INTEGER RANGE 0 TO 5;
-    SIGNAL pulse_widths : pulse_width_array;
+    SIGNAL s_prev_state, s_next_state : state_type;
+    SIGNAL s_cycle_counter : STD_LOGIC_VECTOR(31 DOWNTO 0);
+    SIGNAL s_cycle_counter_total : STD_LOGIC_VECTOR(31 DOWNTO 0);
+    SIGNAL s_channel_index : INTEGER RANGE 0 TO 5;
+    SIGNAL s_pulse_widths : pulse_width_array;
 
 BEGIN
 
@@ -47,63 +35,63 @@ BEGIN
     BEGIN
         IF rising_edge(i_clk) THEN
             IF i_rst = '0' THEN
-                PS <= IDLE;
+                s_prev_state <= IDLE;
             ELSE
-                PS <= NS;
+                s_prev_state <= s_next_state;
             END IF;
         END IF;
     END PROCESS;
 
-    PROCESS (PS, i_rst)
+    PROCESS (s_prev_state, i_rst)
     BEGIN
-        CASE PS IS
-            WHEN CHANNEL =>
-                IF cycle_counter_total < FRAME_COUNT THEN
-                    NS <= CHANNEL;
+        CASE s_prev_state IS
+            WHEN IDLE_PULSE =>
+                IF s_cycle_counter_total < FRAME_COUNT THEN
+                    s_next_state <= IDLE_PULSE;
                 ELSE
-                    NS <= IDLE;
+                    s_next_state <= IDLE;
                 END IF;
 
             WHEN IDLE =>
                 IF i_rst = '0' THEN
-                    NS <= IDLE;
+                    s_next_state <= IDLE;
                 ELSE
-                    NS <= GAP_LOW;
+                    s_next_state <= GAP_LOW;
                 END IF;
 
             WHEN IDLE_LOW_PULSE =>
-                IF cycle_counter >= GAP_COUNT THEN
-                    NS <= CHANNEL;
+                IF s_cycle_counter >= GAP_COUNT THEN
+                    s_next_state <= IDLE_PULSE;
                 ELSE
-                    NS <= IDLE_LOW_PULSE;
+                    s_next_state <= IDLE_LOW_PULSE;
                 END IF;
 
             WHEN GAP_LOW =>
-                IF cycle_counter >= GAP_COUNT THEN
-                    NS <= PULSE_HIGH;
+                IF s_cycle_counter >= GAP_COUNT THEN
+                    s_next_state <= PULSE_HIGH;
                 ELSE
-                    NS <= GAP_LOW;
+                    s_next_state <= GAP_LOW;
                 END IF;
 
             WHEN PULSE_HIGH =>
-                IF (cycle_counter - GAP_COUNT) >= pulse_widths(channel_index)(31 DOWNTO 0) THEN
-                    IF channel_index = 5 THEN
-                        NS <= FRAME_COMPLETE;
+                IF (s_cycle_counter - GAP_COUNT) >= s_pulse_widths(s_channel_index)(31 DOWNTO 0) THEN
+                    IF s_channel_index = 5 THEN
+                        s_next_state <= FRAME_COMPLETE;
                     ELSE
-                        NS <= NEXT_CHANNEL;
+                        s_next_state <= NEXT_CHANNEL;
                     END IF;
                 ELSE
-                    NS <= PULSE_HIGH;
+                    s_next_state <= PULSE_HIGH;
                 END IF;
 
             WHEN NEXT_CHANNEL =>
-                NS <= GAP_LOW;
+                s_next_state <= GAP_LOW;
 
             WHEN FRAME_COMPLETE =>
-                NS <= IDLE_LOW_PULSE;
+                s_next_state <= IDLE_LOW_PULSE;
 
             WHEN OTHERS =>
-                NS <= IDLE;
+                s_next_state <= IDLE;
         END CASE;
     END PROCESS;
 
@@ -111,57 +99,58 @@ BEGIN
     BEGIN
         IF rising_edge(i_clk) THEN
             IF i_rst = '0' THEN
-                cycle_counter <= (OTHERS => '0');
-                cycle_counter_total <= (OTHERS => '0');
-                channel_index <= 0;
+                s_cycle_counter <= (OTHERS => '0');
+                s_cycle_counter_total <= (OTHERS => '0');
+                s_channel_index <= 0;
                 o_ppm <= '1';
                 o_done <= '0';
             ELSE
-                CASE PS IS
-                    WHEN IDLE =>
-                        o_ppm <= '1';
-                        o_done <= '0';
-                        channel_index <= 0;
-                        cycle_counter <= (OTHERS => '0');
-                        cycle_counter_total <= (OTHERS => '0');
+                CASE s_prev_state IS
 
-                    WHEN CHANNEL =>
+                    WHEN IDLE_PULSE =>
                         o_ppm <= '1';
                         o_done <= '1';
-                        channel_index <= 0;
-                        cycle_counter <= (OTHERS => '0');
-                        cycle_counter_total <= cycle_counter_total + 1;
-
-                    WHEN IDLE_LOW_PULSE =>
-                        o_ppm <= '0';
-                        o_done <= '0';
-                        cycle_counter <= cycle_counter + 1;
-                        cycle_counter_total <= cycle_counter_total + 1;
-
-                    WHEN GAP_LOW =>
-                        o_ppm <= '0';
-                        o_done <= '0';
-                        cycle_counter <= cycle_counter + 1;
-                        cycle_counter_total <= cycle_counter_total + 1;
+                        s_channel_index <= 0;
+                        s_cycle_counter <= (OTHERS => '0');
+                        s_cycle_counter_total <= s_cycle_counter_total + 1;
 
                     WHEN PULSE_HIGH =>
                         o_ppm <= '1';
                         o_done <= '0';
-                        cycle_counter <= cycle_counter + 1;
-                        cycle_counter_total <= cycle_counter_total + 1;
+                        s_cycle_counter <= s_cycle_counter + 1;
+                        s_cycle_counter_total <= s_cycle_counter_total + 1;
+
+                    WHEN IDLE =>
+                        o_ppm <= '1';
+                        o_done <= '0';
+                        s_channel_index <= 0;
+                        s_cycle_counter <= (OTHERS => '0');
+                        s_cycle_counter_total <= (OTHERS => '0');
+
+                    WHEN IDLE_LOW_PULSE =>
+                        o_ppm <= '0';
+                        o_done <= '0';
+                        s_cycle_counter <= s_cycle_counter + 1;
+                        s_cycle_counter_total <= s_cycle_counter_total + 1;
+
+                    WHEN GAP_LOW =>
+                        o_ppm <= '0';
+                        o_done <= '0';
+                        s_cycle_counter <= s_cycle_counter + 1;
+                        s_cycle_counter_total <= s_cycle_counter_total + 1;
 
                     WHEN NEXT_CHANNEL =>
-                        channel_index <= channel_index + 1;
-                        cycle_counter <= (OTHERS => '0');
+                        s_channel_index <= s_channel_index + 1;
+                        s_cycle_counter <= (OTHERS => '0');
                         o_done <= '0';
 
                     WHEN FRAME_COMPLETE =>
-                        cycle_counter <= (OTHERS => '0');
-                        channel_index <= 0;
+                        s_cycle_counter <= (OTHERS => '0');
+                        s_channel_index <= 0;
                         o_done <= '0';
 
                     WHEN OTHERS =>
-                        cycle_counter <= (OTHERS => '0');
+                        s_cycle_counter <= (OTHERS => '0');
                 END CASE;
             END IF;
         END IF;
@@ -171,14 +160,14 @@ BEGIN
     BEGIN
         IF rising_edge(i_clk) THEN
             IF i_rst = '0' THEN
-                pulse_widths <= (OTHERS => (OTHERS => '0'));
+                s_pulse_widths <= (OTHERS => (OTHERS => '0'));
             ELSE
-                pulse_widths(0) <= i_slv_reg20;
-                pulse_widths(1) <= i_slv_reg21;
-                pulse_widths(2) <= i_slv_reg22;
-                pulse_widths(3) <= i_slv_reg23;
-                pulse_widths(4) <= i_slv_reg24;
-                pulse_widths(5) <= i_slv_reg25;
+                s_pulse_widths(0) <= i_slv_reg20;
+                s_pulse_widths(1) <= i_slv_reg21;
+                s_pulse_widths(2) <= i_slv_reg22;
+                s_pulse_widths(3) <= i_slv_reg23;
+                s_pulse_widths(4) <= i_slv_reg24;
+                s_pulse_widths(5) <= i_slv_reg25;
             END IF;
         END IF;
     END PROCESS;
